cdfd8ea7a855431eaff1f0e053f01a8d7ee59c76
Larisa Nichi <asiraln21@gmail.com>
2024-11-08 23:55:48 +0200

    update search page

--- src/components/searchTable.js
+++ src/components/searchTable.js
@@ -7,15 +7,16 @@ import {
 } from '@chakra-ui/react';
 
 import { GrSelect } from 'react-icons/gr';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 // https://hatscripts.github.io/circle-flags/flags/xx.svg
 
 export default function SearchTable({ citiesData }) {
+  const router = useRouter();
   return (
     <>
-      <Box position="relative" w="100%" h="70%">
-        <AbsoluteCenter axis="both" w="60%">
+      <Box position="relative" w="100%" h="65vh">
+        <AbsoluteCenter axis="both" w="70%">
           <Table.Root
             size="lg"
             interactive
@@ -23,39 +24,60 @@ export default function SearchTable({ citiesData }) {
             variant="outline"
             colorPalette="blue"
           >
-            <Table.ColumnGroup>
-              <Table.Column />
-              <Table.Column htmlWidth="40%" />
-              <Table.Column htmlWidth="40%" />
-              <Table.Column />
-              <Table.Column />
-            </Table.ColumnGroup>
             <Table.Header>
               <Table.Row>
-                <Table.ColumnHeader py="2" pl="5">
+                <Table.ColumnHeader py="1.5" pl="5">
                   Flag
                 </Table.ColumnHeader>
                 <Table.ColumnHeader>City Name</Table.ColumnHeader>
                 <Table.ColumnHeader>Country</Table.ColumnHeader>
+                <Table.ColumnHeader>Latitude</Table.ColumnHeader>
+                <Table.ColumnHeader>Longitude</Table.ColumnHeader>
+                <Table.ColumnHeader>Code</Table.ColumnHeader>
                 <Table.ColumnHeader>Select</Table.ColumnHeader>
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              {citiesData.map(({ name, country, country_code, id }) => (
-                <Table.Row key={id}>
-                  <Table.Cell py="1.5" pl="5">
-                    <Image
-                      src={`https://hatscripts.github.io/circle-flags/flags/${country_code.toLowerCase()}.svg`}
-                      h="25px"
-                      w="25px"
-                      fit="contain"
-                    ></Image>
-                  </Table.Cell>
-                  <Table.Cell>{name}</Table.Cell>
-                  <Table.Cell>{country}</Table.Cell>
-                  <Table.Cell>
-                    <Link href="/city">
+              {citiesData.map(
+                ({
+                  name,
+                  country,
+                  latitude,
+                  longitude,
+                  population,
+                  country_code,
+                  id,
+                }) => (
+                  <Table.Row key={id}>
+                    <Table.Cell py="1.2" pl="5">
+                      <Image
+                        src={`https://hatscripts.github.io/circle-flags/flags/${country_code.toLowerCase()}.svg`}
+                        h="25px"
+                        w="25px"
+                        fit="contain"
+                      ></Image>
+                    </Table.Cell>
+                    <Table.Cell>{name}</Table.Cell>
+                    <Table.Cell>{country}</Table.Cell>
+                    <Table.Cell>{latitude}</Table.Cell>
+                    <Table.Cell>{longitude}</Table.Cell>
+                    <Table.Cell>{country_code}</Table.Cell>
+                    <Table.Cell>
                       <IconButton
+                        onClick={() => {
+                          localStorage.setItem(
+                            'selectedCity',
+                            JSON.stringify({
+                              name,
+                              country,
+                              latitude,
+                              longitude,
+                              population,
+                              country_code,
+                            })
+                          );
+                          router.push('/city');
+                        }}
                         type="button"
                         aria-label="select"
                         size="xs"
@@ -64,10 +86,10 @@ export default function SearchTable({ citiesData }) {
                       >
                         <GrSelect />
                       </IconButton>
-                    </Link>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
+                    </Table.Cell>
+                  </Table.Row>
+                )
+              )}
             </Table.Body>
           </Table.Root>
         </AbsoluteCenter>
--- src/pages/city.js
+++ src/pages/city.js
@@ -1,7 +1,20 @@
 import Navigation from '@/components/navigation';
 import { Heading } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 
 export default function City() {
+  const [localSelectedCity, setLocalSelectedCity] = useState({});
+  useEffect(() => {
+    let selectedCity;
+    selectedCity = JSON.parse(localStorage.getItem('selectedCity')) || '';
+    setLocalSelectedCity(selectedCity);
+  }, []);
+  console.log(localSelectedCity);
+  const { name, country, latitude, longitude, population, country_code, id } =
+    localSelectedCity;
+
+  async function weatherData() {}
+
   return (
     <>
       <Navigation />
