e85499a3e95dee79ddd018b87b9f663c798211a8
Larisa Nichi <asiraln21@gmail.com>
2024-12-09 23:22:54 +0200

    Organizes in components Favorites page

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,18 +1,6 @@
+import FavoritesContent from '@/components/compFavorites/favoritesContent';
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-import {
-  Heading,
-  Box,
-  Grid,
-  GridItem,
-  Text,
-  Group,
-  Button,
-  Flex,
-  Center,
-} from '@chakra-ui/react';
-import { Rating } from '@/components/ui/rating';
 
 export default function Favorites() {
   const [favoriteCities, setFavoriteCities] = useState([]);
@@ -20,7 +8,6 @@ export default function Favorites() {
   const [ratingsSaved, setRatingsSaved] = useState([]);
   const { data: session } = useSession();
   const currentUser = session?.user.email;
-  const router = useRouter();
 
   useEffect(() => {
     (async () => {
@@ -32,7 +19,6 @@ export default function Favorites() {
       });
       await fetchUserRatings(currentUser).then((data) => {
         setRatingsSaved(data);
-        console.log('GET: data from ratings API', data);
       });
     })();
   }, [currentUser]);
@@ -40,7 +26,6 @@ export default function Favorites() {
   async function fetchUserFavoriteCities() {
     const res = await fetch('/api/cities');
     const users = await res.json();
-    console.log('users from cities:', users);
     const userData = users.filter((user) => user.email === currentUser);
     return userData;
   }
@@ -62,7 +47,6 @@ export default function Favorites() {
         body: JSON.stringify({ latitude, longitude, email }),
       });
       const result = await response.json();
-      // console.log(result);
       if (response.ok) {
         const newCitiesList = favoriteCities.filter(
           (city) => city.latitude !== latitude && city.longitude !== longitude
@@ -128,118 +112,14 @@ export default function Favorites() {
 
   return (
     <>
-      <Box>
-        <Heading
-          as="h1"
-          color="blue.700"
-          size={{
-            sm: '2xl',
-            xl: '4xl',
-          }}
-          textAlign="center"
-          fontWeight="bold"
-          letterSpacing="wide"
-          lineHeight="1.3"
-          my="10"
-        >
-          Your favorite cities
-        </Heading>
-      </Box>
-      <Grid
-        templateColumns={{
-          base: '1fr',
-          md: 'repeat(2, 1fr)',
-        }}
-        maxW={{
-          base: '90%',
-          sm: '75%',
-          md: '85%',
-          lg: '70%',
-        }}
-        columnGap="6"
-        rowGap="2"
-        mx="auto"
-      >
-        {favoriteCities.map(({ id, name, country, latitude, longitude }) => {
-          const score = getCityScore(latitude, longitude);
-          return (
-            <GridItem key={id}>
-              <Flex
-                gap="6"
-                justifyContent="space-between"
-                alignItems="center"
-                bg="blue.100"
-                py="2"
-                px="5"
-              >
-                <Box textAlign="left">
-                  <Text>
-                    {name}, {country}
-                  </Text>
-                </Box>
-                <Group>
-                  <Button
-                    variant="solid"
-                    colorPalette="blue"
-                    size="sm"
-                    px="2"
-                    onClick={() => {
-                      router.push({
-                        pathname: `/city/${name}`,
-                        query: { country, latitude, longitude },
-                      });
-                    }}
-                  >
-                    View
-                  </Button>
-                  <Button
-                    variant="subtle"
-                    colorPalette="blue"
-                    size="sm"
-                    px="2"
-                    onClick={() =>
-                      deleteCityFromFavorites(latitude, longitude, currentUser)
-                    }
-                  >
-                    Delete
-                  </Button>
-                  {score && (
-                    <Rating
-                      value={score}
-                      readOnly
-                      size="sm"
-                      colorPalette="blue"
-                    />
-                  )}
-                  {score === 0 && (
-                    <Rating
-                      defaultValue={0}
-                      onChange={(e) => {
-                        handleRatingOnChange(e, name, latitude, longitude);
-                      }}
-                      size="sm"
-                      colorPalette="blue"
-                    />
-                  )}
-                </Group>
-              </Flex>
-            </GridItem>
-          );
-        })}
-      </Grid>
-      <Center>
-        <Button
-          variant="solid"
-          size="md"
-          px="4"
-          my="7"
-          onClick={() => {
-            sendRatings();
-          }}
-        >
-          Send your Rating!
-        </Button>
-      </Center>
+      <FavoritesContent
+        favoriteCities={favoriteCities}
+        getCityScore={getCityScore}
+        deleteCityFromFavorites={deleteCityFromFavorites}
+        handleRatingOnChange={handleRatingOnChange}
+        sendRatings={sendRatings}
+      ></FavoritesContent>
+      ;
     </>
   );
 }
